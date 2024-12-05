8823ce08d637ac7851e2346be41ea1ff769a5f06
Nedas Barsteika <100798234+NedasBarsteika@users.noreply.github.com>
2024-12-05 21:58:00 +0200

    added projects and contact me sections, added animations on buttons and skills images, changed about section info

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Header from './components/header'
 import AboutMe from './components/about-me';
 import Skills from './components/skills';
+import Projects from './components/projects';
+import ContactMe from './components/contact-me';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
         <Header />
         <AboutMe />
         <Skills />
+        <Projects />
+        <ContactMe />
+        <Footer />
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 export default function MyButton({ title, link }: { title: string, link: string }) {
     return (
         <a href={link}>
-            <button className="bg-yellow-400 text-black px-5 py-2 rounded-lg mr-2">{title}</button>
+            <button className="bg-yellow-400 text-black px-5 py-2 rounded-lg mr-2 transform transition-transform duration-300 hover:scale-110">{title}</button>
         </a>
     );
 }
